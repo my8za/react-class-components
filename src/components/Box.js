1d904359313c0a25a068bac7d99cebe6be59c9ad
my8za <emilie15@naver.com>
2023-01-23 18:56:55 +0900

    class-component

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 export default class Box extends Component {
+  constructor() {
+    super();
+    this.result = ''
+  }
+
+  getResult = () => {
+    if(
+      this.props.title === 'Computer' &&
+      this.props.result !== 'tie' &&
+      this.props.result !== ''
+    ) {
+      this.result = this.props.result === 'win' ? 'lose' : 'win';
+      
+    } else {
+      this.result = this.props.result;
+    }
+  }
   render() {
+    this.getResult();
     return (
-      <div>
-        Box{this.props.num}
+      <div className={`box ${this.result}`}>
+        <h1>{this.props.title}</h1>
+        <img className='item-img' src={this.props.item && this.props.item.img} alt='img'/>
+        <h2>{this.result}</h2>
       </div>
     )
   }
